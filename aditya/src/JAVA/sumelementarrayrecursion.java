ea90905806e14e82cb2c5d52fd6d7eb68c8d5f2f
ADII73 <adityarouth933@gmail.com>
2024-01-06 11:17:35 +0530

    initiate commit

---
+++ aditya/src/JAVA/sumelementarrayrecursion.java
@@ -0,0 +1,16 @@
+package JAVA;
+
+public class sumelementarrayrecursion {
+    static int sumarr(int arr[],int idx){
+        if(idx==arr.length-1){
+            return arr[idx];
+        }
+        int smallans=sumarr(arr,idx+1);
+        return smallans+arr[idx];
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,10};
+        System.out.println(sumarr(arr,0));
+    }
+}
