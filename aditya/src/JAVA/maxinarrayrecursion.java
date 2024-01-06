bce988138063c2578ee5643c2b5b55ca05ca5ad8
ADII73 <adityarouth933@gmail.com>
2024-01-06 11:12:38 +0530

    initiate commit

---
+++ aditya/src/JAVA/maxinarrayrecursion.java
@@ -0,0 +1,18 @@
+package JAVA;
+
+public class maxinarrayrecursion {
+
+    static int  maxarr(int arr[],int idx){      // big problem - give max of array from idx to end
+        if(idx==arr.length-1){                  // base case - if single array then it is the max(single array is at last idx)
+            return arr[idx];
+        }
+
+        int smallans=maxarr(arr,idx+1);     // recursive - it will give max from idx+1
+        return Math.max(arr[idx],smallans);     // self work - compare between small ans and current idx
+
+    }
+    public static void main(String[] args) {
+        int arr[]={45,65,1,54,350};
+        System.out.println(maxarr(arr,0));
+    }
+}
