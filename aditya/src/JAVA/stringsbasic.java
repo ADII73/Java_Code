b84555c2cc69326eb7322a3b291821c0235dba8d
ADII73 <adityarouth933@gmail.com>
2024-01-18 20:44:43 +0530

    initiate commit

---
+++ aditya/src/JAVA/stringsbasic.java
@@ -0,0 +1,45 @@
+package JAVA;
+import java.util.*;
+
+
+public class stringsbasic {
+    public static void main(String[] args) {
+//        String str="Aditya";
+//        System.out.println(str);
+        Scanner sc=new Scanner(System.in);
+//        String s=sc.next();   // Stops printing after space
+//        System.out.println(s);
+
+
+//        String sr=sc.nextLine();  // Consider space
+//        System.out.println(sr);
+
+//        String str="hello world";
+//        System.out.println(str.length());  // .length() in string(also counts space)  .length in array
+
+//        String str="Aditya";
+//        System.out.println(str.charAt(4));  // gives the character at index
+
+//        String str="Aditya";
+//        System.out.println(str.indexOf('y'));   // gives the index of the character
+
+//        String str="Hello";
+//        String gtr="Kello";
+//        System.out.println(str.compareTo(gtr));  // positve and negative comes based on number of ascii values the characters differentiate
+
+//        String str="Aditya";
+//        System.out.println(str.contains("ya"));   // takes strings only and will check if contains in str or not
+
+//        String str="Aditya";
+//        System.out.println(str.toLowerCase());  // to lowerecse with existing character
+
+//        String str="Aditya";
+//        System.out.println(str.toUpperCase());  // to uppercase with existing character
+
+//        String str="Aditya";
+//        String gtr="Routh";
+//        System.out.println(str.concat(gtr));  // concats two strings without spacce
+
+
+    }
+}
