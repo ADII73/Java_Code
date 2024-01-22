8a3ff87f85a11adca9df7bfc88203b93161299b5
ADII73 <adityarouth933@gmail.com>
2024-01-22 12:08:08 +0530

    initiate commit

---
+++ aditya/src/JAVA/printallsubstring.java
@@ -0,0 +1,13 @@
+package JAVA;
+import java.util.*;
+public class printallsubstring {
+    public static void main(String[] args) {
+        String str="abcd";
+        for (int i = 0; i <=3 ; i++) {
+            for (int j = i+1; j <=4 ; j++) {
+                System.out.print(str.substring(i,j)+" ");
+            }
+
+        }
+    }
+}
