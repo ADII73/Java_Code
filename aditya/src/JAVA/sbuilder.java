2ae34ab6913c4c968058953bf33e2e126c0340fb
ADII73 <adityarouth933@gmail.com>
2024-01-22 12:42:18 +0530

    initiate commit

---
+++ aditya/src/JAVA/sbuilder.java
@@ -0,0 +1,26 @@
+package JAVA;
+import java.util.*;
+public class sbuilder {
+    public static void main(String[] args) {
+        StringBuilder str=new StringBuilder("Hello");
+        str.append("world");     // to add strings
+        System.out.println(str);
+        str.setCharAt(0,'M');   // to change character of string which cannot be done in strings but in stringBuilder
+        System.out.println(str);
+
+        str.insert(2,'y');   // to insert character at index
+        System.out.println(str);
+
+        str.deleteCharAt(2);
+        System.out.println(str);    // to delete the ch at index
+
+        StringBuilder s=new StringBuilder("Aditya");
+        System.out.println(s);
+        s.reverse();    // to reverse
+        System.out.println(s);
+
+        s.reverse();
+        s.delete(1,4);
+        System.out.println(s);
+    }
+}
