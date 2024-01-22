91df6564c5b52efd8073837ab973ee9ef2ad1765
ADII73 <adityarouth933@gmail.com>
2024-01-22 10:50:59 +0530

    initiate commit

---
+++ aditya/src/JAVA/stringintchar.java
@@ -0,0 +1,13 @@
+package JAVA;
+import java.util.*;
+public class stringintchar {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    String str="Aditya";
+    str+='R';
+    str+=70;
+        System.out.println(str);
+
+    }
+}
